4d0699576f2173f4b0ca5fa96d7580866f5fca79
Joever Monceda <joever@lbftravel.com>
2019-06-24 23:41:45 +0700

    update values controller use routecontroller

--- server/Controllers/ValuesController.cs
+++ server/Controllers/ValuesController.cs
@@ -3,12 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using server.Helpers;
 
 namespace server.Controllers
 {
     [Route("api/[controller]")]
-    [ApiController]
-    public class ValuesController : ControllerBase
+    public class ValuesController : RouteController
     {
         // GET api/values
         [HttpGet]
